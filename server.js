570cc459a1fc596b5f27efa386a85045a3e0d678
Tanmya Vishvakarma <tanmya2000@gmail.com>
2020-10-31 20:58:08 +0430

    v8

--- server.js
+++ server.js
@@ -19,14 +19,40 @@ app.post('/addproject',function(req,res){
     const brand=req.body.brand;
     const modelname=req.body.modelname;
  
-
-  const sqlinsert="INSERT INTO devices (serialno,brand,modelname) VALUES (?,?,?)"
-  connection.query(sqlinsert,[serialno,brand,modelname],(err,result)=>{
+    const sqlinsert="INSERT INTO devices (serialno,brand,modelname) VALUES (?,?,?)"
+    connection.query(sqlinsert,[serialno,brand,modelname],(err,result)=>{
 
     });
     res.send('add');
 });
 
+app.post('/signup',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+  
+
+    const sqlreg="INSERT INTO users (username,password) VALUES(?,?)"
+    connection.query(sqlreg,[username,password],(err,result)=>{
+
+    });
+    res.send('user registered');
+});
+
+app.post('/signin',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const sqllog="SELECT * FROM users WHERE username=? AND password=?"
+    connection.query(sqllog,[username,password],(err,result)=>{
+        if(result.length>0){
+            res.send(result);
+        }else{
+            res.send({message:"wrong username or password"});
+        }
+
+    });
+    res.send('user registered');
+});
 app.listen(3000,()=>{
     console.log("running");
 });
